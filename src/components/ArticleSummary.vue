b2a125a2f075f45406a384ac9ec35ca73a0e4301
sma2lbao <sma2lbao@qq.com>
2017-04-14 18:00:02 +0800

    update

---
+++ src/components/ArticleSummary.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+    <div class="">
+        <h1>01. First Thoughts on a Thesis</h1>
+        <h2>How We've Evolved, And How Our Extensions Need To</h2>
+        <span><i></i>Mar 29,2012</span>
+        <p>I’ve been thinking about evolution. After hundreds of thousands of years, we’ve become highly physical beings: gangly arms, upright posture, joints and bones that withstand long arduous walks, teeth that can cut and grind nearly anything, hands that can perform millions of coordinated, nuanced manipulations. We’re amazing physical beings. Yet, we spend all of our time sitting, staring into projected light computer terminals, handling virtual objects with a “digital finger” that has less nuance than my pinky toe. We punch 78 or so keys repeatedly to “talk” to friends or to “compose” a piece of music. We’re not using our bodies, and so they are failing us.
+        </p>
+        <span><i></i>2Comments</span>
+        <span><i></i>Music</span>
+        <span><i></i>SVA</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'articleSummary',
+}
+</script>
+
+<style lang="css">
+</style>
